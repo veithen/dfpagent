607847fdc14b27675f86f08cd7c1b61669bd58a1
Andreas Veithen <andreas.veithen@gmail.com>
2013-03-19 14:27:42 +0100

    Removed the code for DFP messages that are not supported by CSS

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ dfpagent
 ========
 
 DFP Agent for WebSphere
+
+
+
+Implemented DFP features
+------------------------
+
+The DFP Agent developed by this project implements the subset of the Dynamic Feedback Protocol that is used by
+Cisco Content Service Switches (CSS). This includes the following message types:
+ * DFP Parameters (inbound)
+ * Preference Information (outbound)
+The agent doesn't support the following message types:
+ * Server State: unfortunately CSS never sends Server State messages
+ * BindID Request, BindID Report and BindID Change Notify: CSS doesn't seem to make use of the Bind-ID concept
--- dfpagent/src/com/github/veithen/dfpagent/protocol/agent/Peer.java
+++ dfpagent/src/com/github/veithen/dfpagent/protocol/agent/Peer.java
@@ -67,11 +67,6 @@ final class Peer implements Runnable, Handler {
     public void processMessage(Message message) throws IOException {
         try {
             switch (message.getType()) {
-                case SERVER_STATE:
-                    TLV loadTLV = message.getRequiredTLV(Type.LOAD);
-                    // TODO
-                    System.out.println("Got a Server State message");
-                    break;
                 case DFP_PARAMETERS:
                     TLV keepAliveTLV = message.getRequiredTLV(Type.KEEP_ALIVE);
                     if (keepAliveTLV.getDataLength() != 4) {
@@ -83,42 +78,6 @@ final class Peer implements Runnable, Handler {
                         Tr.debug(TC, "Got new keep-alive value from peer {0}: {1}", new Object[] { identifier, keepAlive });
                     }
                     keepAliveManager.setKeepAlive(keepAlive);
-                    break;
-                case BINDID_REQUEST:
-                    // Spec: "A separate BindID TLB message should be sent for each DFP
-                    // Agent's real server tables."
-                    for (WeightInfo weightInfo : agent.getWeightInfoProvider().getWeightInfo()) {
-                        TLV tlv = new TLV(Type.BINDID_TABLE);
-                        ValueWriter out = tlv.getValueWriter();
-                        // ** Server IP Address **
-                        out.write(weightInfo.getAddress().getAddress());
-                        // ** Server Port Number **
-                        out.writeShort(weightInfo.getPort());
-                        // ** Protocol **
-                        out.writeByte(0);
-                        // ** Reserved **
-                        out.writeByte(0);
-                        // ** Number of Entries **
-                        out.writeShort(1);
-                        // ** Reserved **
-                        out.writeShort(0);
-                        // ** BindID **
-                        out.writeShort(0);
-                        // ** Reserved **
-                        out.writeShort(0);
-                        // ** Client Network IP Address **
-                        out.writeLong(0);
-                        // ** Netmask of Client Network **
-                        out.writeLong(0);
-                        out.commit();
-                        connection.sendMessage(new Message(MessageType.BINDID_REPORT, tlv));
-                    }
-                    // Spec: "When the entire table has been sent (or
-                    // there is no table to send), this message should be sent with the
-                    // BindID table TLV entries with a zero Server IP address, Server port,
-                    // protocol, and number of entries.  This indicates there is no more
-                    // data to be sent."
-                    connection.sendMessage(new Message(MessageType.BINDID_REPORT, new TLV(Type.BINDID_TABLE, new byte[12])));
                     break;
                 default:
                     Tr.warning(TC, Messages._0007W, message.getType().getName());
